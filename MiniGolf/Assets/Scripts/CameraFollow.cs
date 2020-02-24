b18414f9273e740f83aa25c8897af04dce99c175
Callum Metcalfe <callummgamedev@hotmail.com>
2020-02-24 14:35:46 +0000

    Created basic camera script

---
+++ MiniGolf/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private float zoomSpeed;
+    [SerializeField]
+    private GameObject targetObj;
+    [SerializeField]
+    private float rotationSpeed;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = targetObj.transform;
+        transform.LookAt(target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Zoom in and Zoom Out
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(target.position, target.up, Time.deltaTime * rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(target.position, -target.up, Time.deltaTime * rotationSpeed);
+
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.RotateAround(target.position, target.forward, Time.deltaTime * rotationSpeed);
+            //this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, this.transform.rotation.y, 0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.RotateAround(target.position, -target.forward, Time.deltaTime * rotationSpeed);
+        }
+    }
+}
