f092a810f1de50b4a51aa29b852d850b555eb583
Callum Metcalfe <callummgamedev@hotmail.com>
2020-03-09 17:03:06 +0000

    added a way to switch between controller input and keyboard input

--- Assets/Scripts/Input/BallManager.cs
+++ Assets/Scripts/Input/BallManager.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallManager : MonoBehaviour
-{ 
+{
+    [SerializeField]
+    private bool ControllerEnabled = false;
 
+    private void Start()
+    {
+        if (ControllerEnabled)
+        {
+            this.GetComponent<keyboardControlls>().enabled = false;
+        }
+        else if (!ControllerEnabled)
+        {
+            this.GetComponent<ArduinoControls>().enabled = false;
+        }
+    }
 }
