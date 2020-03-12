3787d072c662d7390465164c0265ab15e74399a8
Callum Metcalfe <cm227781@falmouth.ac.uk>
2020-03-12 14:43:27 +0000

    messing with calibrating gyro

--- Assets/Scripts/Input/ArduinoControls.cs
+++ Assets/Scripts/Input/ArduinoControls.cs
@@ -8,7 +8,30 @@ using System.Collections;
 public class ArduinoControls : MonoBehaviour
 {
     private SerialPort serialPort;
+    [SerializeField]
+    private int xAccelOffset;
+    [SerializeField]
+    private int yAccelOffset;
+    [SerializeField]
+    private int zAccelOffset;
+    [SerializeField]
+    private int xGyroOffset;
+    [SerializeField]
+    private int yGyroOffset;
+    [SerializeField]
+    private int zGyroOffset;
 
+    private int xAccel;
+    private int yAccel;
+    private int zAccel;
+    private int xGyro;
+    private int yGyro;
+    private int zGyro;
+
+    private string serialRead;
+    private string[] neededValues;
+    private int length;
+    private int[] asInt;
     private void Start()
     {
         serialPort = new SerialPort("COM3", 115200);
@@ -17,10 +40,44 @@ public class ArduinoControls : MonoBehaviour
         {
             Debug.Log("Open");
         }
+
         serialPort.ReadTimeout = 20;
+
     }
     private void Update()
     {
-        Debug.Log("Read " + serialPort.ReadLine());
+        serialRead = serialPort.ReadLine();
+        neededValues = serialRead.Split();
+        length = neededValues.Length;
+        int result;
+        for (int i = 0; i < length; i++)
+        {
+            int.TryParse(neededValues[i], out result);
+            if (i == 0)
+            {
+                xAccel = result + xAccelOffset;
+            }
+            if (i == 1)
+            {
+                yAccel = result + yAccelOffset;
+            }
+            if (i == 2)
+            {
+                zAccel = result + zAccelOffset;
+            }
+            if (i == 3)
+            {
+                xGyro = result + xGyroOffset;
+            }
+            if (i == 4)
+            {
+                yGyro = result + yGyroOffset;
+            }
+            if (i == 5)
+            {
+                zGyro = result + zGyroOffset;
+            }
+        }
+        Debug.Log(xAccel + " " + yAccel + " " + zAccel + " " + xGyro + " " + yGyro + " " + zGyro);
     }
 }
